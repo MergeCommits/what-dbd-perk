33c9df788c47ed30147cb5112e7f04a24699f61e
Mark Jarjour <markjarjour@gmail.com>
2023-04-07 13:20:14 -0400

    Updated t3 app.

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -1,10 +1,20 @@
 import { initTRPC } from "@trpc/server";
 import superjson from "superjson";
+import { ZodError } from "zod";
 
 const t = initTRPC.context().create({
     transformer: superjson,
-    errorFormatter({ shape }) {
-        return shape;
+    errorFormatter({ shape, error }) {
+        return {
+            ...shape,
+            data: {
+                ...shape.data,
+                zodError:
+                    error.cause instanceof ZodError
+                        ? error.cause.flatten()
+                        : null,
+            },
+        };
     },
 });
 
