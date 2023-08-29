da65b48873abb2b584bcdf439ea8438f3af4cfdd
Mark Jarjour <markjarjour@gmail.com>
2023-08-29 16:27:44 -0400

    Removed code property from perks. Perk name is no longer required for getting the image URL

--- src/pure/getPerksWithTags.ts
+++ src/pure/getPerksWithTags.ts
@@ -2,8 +2,22 @@ import { env } from "env.mjs";
 import { getPerkDescriptionFromWiki } from "pure/getPerkDescriptionFromWiki";
 import { getAllPerks } from "pure/perkInfo/getAllPerks";
 
-function getImageURL(perkName: string, imageCode: string) {
-    return `https://static.wikia.nocookie.net/deadbydaylight_gamepedia_en/images/${imageCode}.png`;
+function getImageURL(
+    perkName: string,
+    imageCode: string,
+    perkNameInImage?: string
+) {
+    const perkNameInFile =
+        perkNameInImage ??
+        perkName
+            .replace(/\w/g, (match) => match.toLowerCase())
+            .replace(/&/g, "and")
+            .replace(/[^\w\s-]/g, "")
+            .replace(/\W(\w)/g, (match) => match.toUpperCase())
+            .replace(/^(.)/, (match) => match.toLowerCase())
+            .replace(/\s/g, "");
+    const fileName = `IconPerks_${perkNameInFile}.png`;
+    return `https://static.wikia.nocookie.net/deadbydaylight_gamepedia_en/images/${imageCode}/${fileName}`;
 }
 
 export async function getPerksWithTags(tags: string[]) {
@@ -18,7 +32,7 @@ export async function getPerksWithTags(tags: string[]) {
 
     const promises = filteredPerks.map(async (perk) => ({
         ...perk,
-        icon: getImageURL(perk.name, perk.icon),
+        icon: getImageURL(perk.name, perk.icon, perk.perkNameInImage),
         description: await getPerkDescriptionFromWiki(perk.name).catch((e) => {
             // eslint-disable-next-line no-console
             console.error(
